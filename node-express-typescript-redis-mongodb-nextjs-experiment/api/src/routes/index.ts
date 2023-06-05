17ff9f79b779d71602c3b80c468915c97a610d08
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-06 03:49:16 +0600

    connect signup route

--- node-express-typescript-redis-mongodb-nextjs-experiment/api/src/routes/index.ts
+++ node-express-typescript-redis-mongodb-nextjs-experiment/api/src/routes/index.ts
@@ -1,13 +1,15 @@
 import { Application } from 'express';
-import userRouter from './user.route';
-import otpRoute from './otp.route';
-import taskRouter from './task.route';
+// import userRouter from './user.route';
+// import otpRoute from './otp.route';
+// import taskRouter from './task.route';
+import {authRoutes} from '../modules/auth/routes/auth.route';
 
+const BASE_PATH = '/api/v1/';
 const appRoutes = (app: Application) => {
   const routes = () => {
-    app.use('/api/v1', userRouter);
-    app.use('/api/v1', otpRoute);
-    app.use('/api/v1', taskRouter);
+    app.use(BASE_PATH, authRoutes.routes());
+    // app.use(BASE_PATH, otpRoute);
+    // app.use(BASE_PATH, taskRouter);
   };
   routes();
 };
