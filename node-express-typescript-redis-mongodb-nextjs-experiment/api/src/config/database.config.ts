34538722befd4188a8975a0cd45cacee11f84e65
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-06 03:51:35 +0600

    update database config

--- node-express-typescript-redis-mongodb-nextjs-experiment/api/src/config/database.config.ts
+++ node-express-typescript-redis-mongodb-nextjs-experiment/api/src/config/database.config.ts
@@ -2,15 +2,17 @@ import mongoose from 'mongoose';
 import { envConfig } from './env.config';
 import Logger from 'bunyan';
 import { logConfig } from './log.config';
+import {redisConnection} from '../global/redis/redis.connection';
 const log: Logger = logConfig.createLogger('server');
 export default () => {
-  let URI = envConfig.DATABASE_URL;
-  let OPTION = { user: '', pass: '', autoIndex: true };
+  const URI = envConfig.DATABASE_URL;
+  const OPTION = { user: '', pass: '', autoIndex: true };
   const connect = () => {
     mongoose
       .connect(URI, OPTION)
       .then((res) => {
         log.info('Database Connection Successful!');
+        redisConnection.connect();
       })
       .catch((err) => {
         log.error('Database Connection Failed!', err);
