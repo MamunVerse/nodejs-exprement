31e62d528e33f8c18b266f2d6e9bd8fffe207a75
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-06 03:50:13 +0600

    made auth router

---
+++ node-express-typescript-redis-mongodb-nextjs-experiment/api/src/modules/auth/routes/auth.route.ts
@@ -0,0 +1,15 @@
+import express, {Router} from 'express';
+import {SignUp} from '../controllers/signup';
+class AuthRoutes {
+  private readonly router : Router;
+  constructor() {
+    this.router = express.Router();
+  }
+  public routes() : Router {
+    this.router.post('/signup', SignUp.prototype.create);
+
+    return this.router;
+  }
+}
+
+export const authRoutes : AuthRoutes = new AuthRoutes();
