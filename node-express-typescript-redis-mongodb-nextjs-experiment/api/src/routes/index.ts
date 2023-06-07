368281cc2297014be164f762444ed021b811daea
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-07 06:28:26 +0600

    update index

--- node-express-typescript-redis-mongodb-nextjs-experiment/api/src/routes/index.ts
+++ node-express-typescript-redis-mongodb-nextjs-experiment/api/src/routes/index.ts
@@ -3,10 +3,12 @@ import { Application } from 'express';
 // import otpRoute from './otp.route';
 // import taskRouter from './task.route';
 import {authRoutes} from '../modules/auth/routes/auth.route';
+import {serverAdapter} from '../global/queues/base.queue';
 
 const BASE_PATH = '/api/v1/';
 const appRoutes = (app: Application) => {
   const routes = () => {
+    app.use('/queues', serverAdapter.getRouter());
     app.use(BASE_PATH, authRoutes.routes());
     // app.use(BASE_PATH, otpRoute);
     // app.use(BASE_PATH, taskRouter);
