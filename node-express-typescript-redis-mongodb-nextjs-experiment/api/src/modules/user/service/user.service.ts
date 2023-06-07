551a7e3decb2061f6e8ef59cb32c1ff6f22bca19
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-08 03:18:46 +0600

    Add service methods  for  getUserByAuthId

--- node-express-typescript-redis-mongodb-nextjs-experiment/api/src/modules/user/service/user.service.ts
+++ node-express-typescript-redis-mongodb-nextjs-experiment/api/src/modules/user/service/user.service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import {UserModel} from '../models/user.schema';
 import {IUserDocument} from '../interface/user.interface';
 
@@ -5,6 +6,45 @@ import {IUserDocument} from '../interface/user.interface';
 class UserService {
   public async addUserData(data: IUserDocument): Promise<void> {
     await UserModel.create(data);
+  }
+
+  public async getUserByAuthId(authId: string): Promise<IUserDocument> {
+
+    const id  =  new mongoose.Types.ObjectId(authId);
+
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { authId: id } },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $project: this.aggregateProject() }
+    ]);
+
+    return users[0];
+  }
+
+  private aggregateProject() {
+    return {
+      _id: 1,
+      username: '$authId.username',
+      uId: '$authId.uId',
+      email: '$authId.email',
+      avatarColor: '$authId.avatarColor',
+      createdAt: '$authId.createdAt',
+      postsCount: 1,
+      work: 1,
+      school: 1,
+      quote: 1,
+      location: 1,
+      blocked: 1,
+      blockedBy: 1,
+      followersCount: 1,
+      followingCount: 1,
+      notifications: 1,
+      social: 1,
+      bgImageVersion: 1,
+      bgImageId: 1,
+      profilePicture: 1
+    };
   }
 
 }
