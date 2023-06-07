39dcc60993476de7249c666a8e3e8a50f798caef
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-07 06:29:30 +0600

    made user worker

---
+++ node-express-typescript-redis-mongodb-nextjs-experiment/api/src/global/workers/user.worker.ts
@@ -0,0 +1,23 @@
+import { DoneCallback, Job } from 'bull';
+import Logger from 'bunyan';
+import {logConfig} from '../../config/log.config';
+import {userService} from '../../modules/user/service/user.service';
+
+const log: Logger = logConfig.createLogger('userWorker');
+
+class UserWorker {
+  async addUserToDB(job: Job, done: DoneCallback): Promise<void> {
+    try {
+      const {value} = job.data;
+      await userService.addUserData(value);
+      job.progress(100);
+      done(null, job.data);
+    } catch (error) {
+      log.error(error);
+      done(error as Error);
+    }
+  }
+}
+
+
+export const userWorker: UserWorker = new UserWorker();
