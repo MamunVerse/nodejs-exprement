6e50fbbe6d03111f43104d2f995f24eaaf4d5cb2
Mamunur Rashid <mamunurrashid6424@gmail.com>
2023-06-07 06:28:55 +0600

    made auth service

--- node-express-typescript-redis-mongodb-nextjs-experiment/api/src/modules/auth/services/auth.service.ts
+++ node-express-typescript-redis-mongodb-nextjs-experiment/api/src/modules/auth/services/auth.service.ts
@@ -3,6 +3,10 @@ import {Helpers} from '../../../utility/helper.utility';
 import {AuthModel} from '../models/auth';
 
 class AuthService {
+
+  public async createAuthUser(data: IAuthDocument): Promise<void> {
+    await AuthModel.create(data);
+  }
   public async getUserByUsernameOrEmail(username: string, email: string): Promise<IAuthDocument> {
     const query = {
       $or: [
